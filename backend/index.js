f04c6719fb7fad7510c8d6c38944d6f714e77d57
Rathagithub <rathakrishnait@gmail.com>
2022-01-12 16:10:30 +0530

    added proxy

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const cors = require('cors')
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/Router');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,4 +16,10 @@ app.listen(port, () => {
  console.log("Server is running on port 8001.");
 });
 
-module.exports = app
+
+
+module.exports = function (app) {
+ app.use(createProxyMiddleware('/api/**', {
+  target: 'http://localhost:8001'
+ }));
+};
